dc72f9df43201f034b14a3be1dbb7b24c5b036a1
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-07-09 18:58:39 -0500

    base de modulo empleado

---
+++ src/modules/RRHH/use-cases/empleado/crearEmpleado.UseCase.ts
@@ -0,0 +1,31 @@
+import { PrismaService } from '@/common/prisma/prisma.service';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  dtoCreateEmpleado,
+  createEmpleadoSchema,
+} from '@jyp/shared-scontracts';
+
+@Injectable()
+export class CrearEmpleadoUseCase {
+  constructor(private readonly prisma: PrismaService) {}
+  async execute(dto: dtoCreateEmpleado) {
+    //validar con zod
+    const data = createEmpleadoSchema.parse(dto);
+    try {
+      //crear empleado con prisma
+      const empleadoCreado = await this.prisma.empleados.create({ data: data });
+      return {
+        state: true,
+        message: 'Empleado creado correctamente',
+        data: empleadoCreado,
+      };
+    } catch (error) {
+      throw new BadRequestException({
+        type: 'https://api.jyp.com/errors/bad-request',
+        title: 'Error al crear el empleado',
+        detail:
+          'No se pudo guardar en la base de datos. Verifica que el nombre no esté duplicado.',
+      });
+    }
+  }
+}
---
+++ src/modules/RRHH/use-cases/empleado/index.ts
@@ -0,0 +1,1 @@
+export * from './crearEmpleado.UseCase';
