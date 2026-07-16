5cb8470f756246294c806911c940f37c73673df9
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-07-15 19:53:49 -0500

    feature: arreglo de rutas de area controller

--- src/modules/RRHH/controller/Area.controller.ts
+++ src/modules/RRHH/controller/Area.controller.ts
@@ -5,14 +5,14 @@ import {
   Body,
   HttpCode,
   HttpStatus,
-  Put,
   Param,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 //validacion de estructura de datos mediate el zod
 import type {
   dtoCrearAreaInput,
   dtoActualizarAreaInput,
-  dtoEliminarAreaInput,
 } from '@jyp/shared-contracts';
 //casos de uso
 import {
@@ -30,13 +30,25 @@ export class AreaController {
     private readonly eliminarAreaUseCase: EliminarAreaUseCase,
   ) {}
   //delete - softdelete
-  @Put('eliminar')
+  /**
+   * @param id: string
+   * @returns {
+   *
+   * }
+   * @URL : http://localhost:3000/api/rrhh/area/ @Param /active
+   */
+  @Delete(':id/active')
   @HttpCode(HttpStatus.OK)
-  async eliminar(@Body() payload: dtoEliminarAreaInput) {
-    return await this.eliminarAreaUseCase.execute(payload);
+  async eliminar(@Param('id') id: string) {
+    return await this.eliminarAreaUseCase.execute(id);
   }
   //update - total
-  @Put('actualizar/:id')
+  /**
+   * @param id
+   * @param payload
+   * @returns
+   */
+  @Patch('actualizar/:id')
   @HttpCode(HttpStatus.OK)
   async update(
     @Param('id') id: string,
@@ -45,6 +57,10 @@ export class AreaController {
     return await this.updateAreaUseCase.execute(id, payload);
   }
   //post - crear
+  /**
+   * @param payload
+   * @returns
+   */
   @Post('crear')
   @HttpCode(HttpStatus.CREATED)
   async crear(@Body() payload: dtoCrearAreaInput) {
