917b342a8db85aa1c91a7ecb63593aeebdc2ef86
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-08-07 12:33:03 -0500

    feat: obtener datos financiero de empleado

--- src/modules/dato_financiero/use-case/obtenerDatosEmpleado.useCase.ts
+++ src/modules/dato_financiero/use-case/obtenerDatosEmpleado.useCase.ts
@@ -1,5 +1,9 @@
 import { PrismaService } from '@/common/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
 export class obtenerDatosEmpleadoUseCase {
@@ -10,6 +14,56 @@ export class obtenerDatosEmpleadoUseCase {
       where: {
         id: idEmpleado,
       },
+      select: {
+        id: true,
+        activo: true,
+        deleted_at: true,
+      },
     });
+    //validar empleado
+    if (!empleado || empleado.deleted_at !== null || empleado.activo !== true) {
+      throw new NotFoundException('empleado no encontrado o inactivo');
+    }
+    try {
+      //datos financiero de empleado
+      const daots_financiero = await this.prisma.dato_financiero.findUnique({
+        where: {
+          empleado_id: empleado.id,
+          deleted_at: null,
+        },
+        select: {
+          id: true,
+          cuenta_bancaria: true,
+          sueldo_basico: true,
+          cuspp: true,
+          tipo_comision: true,
+          cci: true,
+          nro_cuenta_cts: true,
+          empleados: {
+            select: {
+              id: true,
+              nombre: true,
+              apellido: true,
+            },
+          },
+          regimen_pension: {
+            select: {
+              nombre: true,
+            },
+          },
+          bancos: {
+            select: {
+              nombre: true,
+            },
+          },
+        },
+      });
+      return daots_financiero;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'error al ingresar datos financieros de empleado',
+        error instanceof Error ? error.message : String(error),
+      );
+    }
   }
 }
